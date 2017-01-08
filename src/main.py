d45852334c4a0d8a891f8c0c9d92087ee212e1da
jamesread <contact@jwread.com>
2017-01-08 00:02:29 +0000

    Heartbeater.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from upsilon import logger, amqp, sql
 from config import RuntimeConfig
 from MessageHandler import MessageHandler
 
+logger.log("upsilon-reactor")
+
 config = RuntimeConfig()
 
 messageHandler = MessageHandler()
@@ -15,11 +17,13 @@ messageHandler.setMySqlConnection(mysqlConnection)
 
 amqpConnection = amqp.Connection(config.amqpHost, config.amqpQueue, config.amqpExchange);
 amqpConnection.setPingReply("reactor", "devel", "db, amqp, reactor");
-messageHandler.setAmqpConnection(amqpConnection)
+amqpConnection.startHeartbeater();
 
-logger.log("upsilon-node-alerter")
+messageHandler.setAmqpConnection(amqpConnection)
 
 try:
     messageHandler.start();
-except KeyboardInterrupt:
-    messageHandler.amqp.close()
+except KeyboardInterrupt as e:
+    print e
+
+print "stop"
