2e88529b3ad7bfc419de93d1d0fbe9e90977b625
jamesread <contact@jwread.com>
2017-01-08 00:25:12 +0000

    Fixup on ping reply.

--- src/MessageHandler.py
+++ src/MessageHandler.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python
 
 class MessageHandler():
-	amqp = None
-	mysql = None
+    amqp = None
+    mysql = None
 
-	def setMySqlConnection(self, mysqlConnection):
-		self.mysql = mysqlConnection
-	
-	def setAmqpConnection(self, amqpConnection):
-		self.amqp = amqpConnection
-                self.amqp.bindEverything()
-                self.amqp.setPingReply('upsilon-reactor', 'devel', 'db, amqp')
-                self.amqp.addMessageTypeHandler('HEARTBEAT', self.onHeartbeat)
+    def setMySqlConnection(self, mysqlConnection):
+        self.mysql = mysqlConnection
+    
+    def setAmqpConnection(self, amqpConnection):
+        self.amqp = amqpConnection
+        self.amqp.bindEverything()
+        self.amqp.addMessageTypeHandler('HEARTBEAT', self.onHeartbeat)
+        self.amqp.addMessageHandler(self.onMessage)
 
-        def start(self):
-                self.amqp.startConsuming()
+    def start(self):
+        self.amqp.startConsuming()
 
-	def onHeartbeat(self, channel, method, properties, body):
-                print "Heartbeat"
+    def onHeartbeat(self, channel, method, properties, body):
+        print "Heartbeat"
 
-		channel.basic_ack(delivery_tag = method.delivery_tag, multiple = False)
+        channel.basic_ack(delivery_tag = method.delivery_tag, multiple = False)
+
+    def onMessage(self, channel, method, properties, body):
+        print "msg"
+        channel.basic_ack(delivery_tag = method.delivery_tag, multiple = False)
